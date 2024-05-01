d47491203c173085ae6db2ee37cc806d8ac2f571
dimaluts2023 <dima.luts.standoff@gmail.com>
2024-05-01 22:52:03 +0200

    Lesson 1

---
+++ lesson 1/js/script.js
@@ -0,0 +1,32 @@
+let var1 = "hello"
+let var2 = "owu"
+let var3 = "com"
+let var4 = "ua"
+let var5 = 1
+let var6 = 10
+let var7 = -999
+let var8 = 123
+let var9 = 3.14
+let var10 = 2.7
+let var11 = 16
+let var12 = true
+let var13 = false
+
+console.log(var1, var2, var3, var4, var5, var6, var7, var8, var9, var10, var11, var12, var13)
+
+let firstName = 'Дмитро'
+let middleName = 'Васильович'
+let lastName = 'Луць'
+let person = lastName + ' ' + firstName + ' ' + middleName
+console.log(person)
+
+let a = 100; let b = '100'; let c = true;
+console.log("a: " + typeof(a))
+console.log("b: " + typeof(b))
+console.log("c: " + typeof(c))
+
+const name = prompt('Your Name')
+const middle = prompt("Middle name")
+const age = prompt("Age")
+
+console.log(name + ' ' + middle + ' ' + age)
